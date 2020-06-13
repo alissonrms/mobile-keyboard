f7e1c08c8a9fafb399724ff077dd94aeeb025915
alissonrms <armelquiades2000@gmail.com>
2020-06-12 21:42:48 -0300

    Iniciando a comunicação da aplicação desktop com o servidor

--- desktop/main.js
+++ desktop/main.js
@@ -2,22 +2,40 @@ const path = require('path')
 const { app, Menu, Tray } = require('electron')
 
 
-
 let tray = null
 app.on('ready', () => {
-  function connectToServer(){
-    console.log('conectar ao servidor')
+
+  function connectToServer() {
+    console.log('trying connect to the server')
+    const { net } = require('electron')
+
+    const request = net.request('http://localhost:3333')
+
+    request.setHeader("usertype", "desktop")
+    request.setHeader("password", "batatateste")
+
+
+    request.on("response", (response) => {
+      response.on('data', (response) => {
+        const ResponseObject = JSON.parse(`${response}`)
+        console.log(ObjectResponse.status)
+      })
+      response.on('end', () => {
+      })
+    })
+    request.end()
+
   }
-  
-  function closeApp(){
+
+  function closeApp() {
     tray.destroy()
-    app.quit() 
+    app.quit()
   }
 
   tray = new Tray(path.resolve(__dirname, 'assets', 'icon.png'))
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Conectar', click: connectToServer},
-    { label: 'Fechar', click: closeApp}
+    { label: 'Conectar', click: connectToServer },
+    { label: 'Fechar', click: closeApp }
   ])
   tray.setToolTip('Teclado móvel')
   tray.setContextMenu(contextMenu)
